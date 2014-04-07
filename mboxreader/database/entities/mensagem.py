d0bc8fd08aef5dbdbf0c33247aeddb30c957f14b
Igor Steinmacher <igorfs@utfpr.edu.br>
2014-04-07 14:58:06 -0300

    Just testing

--- mboxreader/database/entities/mensagem.py
+++ mboxreader/database/entities/mensagem.py
@@ -9,13 +9,14 @@ from storm.properties import Int, Unicode, DateTime
 class Mensagem(object):
     __storm_table__ = "mensagem"
     id = Int(primary=True)
-    message_from = Unicode() #Necessário já que from é uma palavra reservada
+    message_from = Unicode() #Necessï¿½rio jï¿½ que from ï¿½ uma palavra reservada
     to = Unicode()
     subject = Unicode()
     date = DateTime()
     message_id = Unicode()
     in_reply_to = Unicode()
     references = Unicode()
+#Olhe aqui Felipe o que eu comentei
 #    mensagemPai = reference(mensagemPaiId, Mensagem.id)
 #    pessoa = reference(pessoaId, Pessoa.id)
 #    projeto = reference(projetoId, Projeto.id)
